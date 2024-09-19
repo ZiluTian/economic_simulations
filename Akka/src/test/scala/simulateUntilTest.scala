301ccc77988628f1864f6b8df3359867025576d3
Tian Zilu <zilu.tian@uzh.ch>
2024-09-20 00:56:33 +0200

    Update to the correct experiment version

--- Akka/src/test/scala/simulateUntilTest.scala
+++ Akka/src/test/scala/simulateUntilTest.scala
@@ -12,14 +12,14 @@ class simulateUntilTest extends FlatSpec {
     f"The epidemics example" should "stop as soon as the termination condition is met" in {
       val population = 10000
       val graph = cloudcity.lib.Graph.GraphFactory.erdosRenyi(population, 0.01)
-      val agents = generated.example.epidemic.InitData(graph)
+      val agents = generated.example.epidemic.v2.InitData(graph)
       val conf = Map("role" -> "Standalone", 
             "port" -> 25200, 
             "name" -> "Epidemics", 
             "data" -> "timeseries")
       Simulate.apply(agents, totalRounds, conf, Some((ts: Iterable[Iterable[Serializable]]) => {
         val x = ts.last.filter(i => i match {
-          case y: generated.example.epidemic.Person => y.health == 1
+          case y: generated.example.epidemic.v2.Person => y.health == 1
         }).size
         println("Total infected agents: " + x)
         x > population / 2
