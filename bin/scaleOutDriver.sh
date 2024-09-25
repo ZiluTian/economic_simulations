de0283531863826f2075856e98f48178997b2084
Tian Zilu <zilu.tian@uzh.ch>
2024-09-25 20:11:51 +0200

    Update

--- bin/scaleOutDriver.sh
+++ bin/scaleOutDriver.sh
@@ -9,10 +9,28 @@ WORKING_DIR="/home/user/zilu/economic_simulations"
 # Clean up any remaining Bench processes
 stopWorkers
 
-for cmd in "${EXPERIMENTS[@]}"; do
-    # Start the driver
+# Cluster should not take more than 3 minutes to start
+START_CLUSTER_TIMEOUT=180
+MAX_RETRY=5
+
+# Repeat
+repeat=3
+
+startExperiment(){
+    cmd=$1
+    echo "Start experiment $cmd"
+
+    file_path="$LOG/$cmd.log"
+    if [ -f "$file_path" ]; then
+        timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+        mv "$file_path" "${file_path}_$timestamp"
+        echo "$file_path renamed to: ${file_path}_$timestamp"
+    else
+        echo "File does not exist: $file_path"
+    fi
+
     DRIVER_COMMAND="export PATH=$JAVA_HOME:$PATH && cd $WORKING_DIR && bash bin/scaleOutBench.sh 0 $cmd"
-    ssh "$DRIVER_IP" "$DRIVER_COMMAND" &
+    ssh "$DRIVER_IP" "$DRIVER_COMMAND" > $file_path 2>&1 &
     # Wait for the lock on sbt to be availble
     sleep 3
 
@@ -32,19 +50,65 @@ for cmd in "${EXPERIMENTS[@]}"; do
             echo "Command executed successfully on ${WORKERS[$machineId]}"
         fi
     done
+}
 
-    sleep 10
-    # driver
-    tail -n 0 -f "$LOG/$cmd.log" | while read line; do
-        echo "Check if simulation has ended!"
-        if echo "$line" | grep -q "Average"; then
-            echo "Keyword 'Average' found."
-            # Execute the command
-            echo "$line" >> "$LOG/${cmd}_m${TOTAL_WORKERS}"
-            stopWorkers
-            break
-        fi
-        # Wait for 2 seconds before checking the next batch of lines
-        sleep 2
-    done
-done 
+for rpt in $(seq 2 $repeat); do
+    for cmd in "${EXPERIMENTS[@]}"; do
+        # Start the driver
+        startExperiment "$cmd"
+
+        # Restart the cluster if it takes more than 10minutes to start a cluster
+        SECONDS=0
+        failure_count=0
+
+        while true; do
+            echo "Wait for the simulation to start"
+
+            if grep -q "Connection refused" "$LOG/$cmd.log"; then 
+                echo "Connection refused. Retry the experiment"
+                stopWorkers
+                sleep 100
+                startExperiment "$cmd"
+            fi
+
+            # Check the file for the keyword
+            if grep -q "Round 1 takes" "$LOG/$cmd.log"; then
+                echo "Keyword 'Round 1' found! Continue"
+                break
+            fi
+
+            if [ "$SECONDS" -ge "$START_CLUSTER_TIMEOUT" ]; then
+                echo "Timeout reached. Keyword not found. Restarting the experiment"
+                # Run the failure command
+                stopWorkers
+                sleep 100
+                startExperiment "$cmd"
+                failure_count=$((failure_count + 1))
+            fi
+
+            if [ "$failure_count" -ge "$MAX_RETRY" ]; then
+                echo "Max error retry has reached. Exit"
+                exit 1
+            fi
+
+            # Wait 2 seconds before checking again
+            sleep 3
+        done
+
+        sleep 10
+        # driver
+        while true; do
+            echo "Wait for the simulation to stop"
+            # Check the file for the keyword
+            if grep -q "Average" "$LOG/$cmd.log"; then
+                echo "Keyword 'Average' found."
+                # Execute the command
+                mv "$LOG/$cmd.log" "$LOG/${cmd}_m${TOTAL_WORKERS}_r${rpt}"
+                stopWorkers
+                break
+            fi
+            # Wait 1 second before checking again
+            sleep 3
+        done
+    done 
+done
