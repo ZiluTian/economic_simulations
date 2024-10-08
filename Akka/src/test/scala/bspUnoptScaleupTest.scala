51b9dcc3dc93ad3f0cec4502cf2d3505b13a1c37
Tian Zilu <zilu.tian@uzh.ch>
2024-10-08 20:15:09 +0200

    Add unoptimized tests

---
+++ Akka/src/test/scala/bspUnoptScaleupTest.scala
@@ -0,0 +1,78 @@
+package simulation.akka
+package test
+
+import simulation.akka.API._
+import org.scalatest.FlatSpec
+import meta.API.{SimulationData, DeforestationStrategy}
+import cloudcity.lib.Graph._
+import BSPModel._
+import BSPModel.example.gameOfLife._
+import scala.util.Random
+import meta.runtime.{IntVectorMessage, IntArrayMessage, Actor}
+import BSPModel.Connector._
+import BSPModel.example.stockMarket._
+import BSPModel.example.epidemics._
+
+abstract class EpidemicsUnoptTest extends scaleUpTest {
+    override val totalRounds: Int = 50
+
+    def genPopulation(g: Map[Int, Iterable[Int]]): IndexedSeq[BSP with ComputeMethod] = {
+        g.map(i => {
+            val age: Int = Random.nextInt(90)+10
+            new PersonAgent(i._1, 
+                    age, 
+                    i._2.toVector, 
+                    Random.nextBoolean(), 
+                    if (Random.nextInt(100)==0) 0 else 2,
+                    if (age > 60) 1 else 0)
+        }).toVector
+    }
+}
+
+class gameOfLifeUnoptTest extends scaleUpTest {
+
+    def gen(scaleUpFactor: Int): IndexedSeq[Actor] = {
+        val width: Int = 100
+        val height: Int = scaleUpFactor*(baseFactor / width).toInt
+        val graph = GraphFactory.torus2D(width, height)
+        graph.adjacencyList().map(i => bspToAgent(new Cell(i._1.toInt, i._2.map(_.toInt).toSeq))).toVector        
+    }
+}
+
+class stockMarketUnoptTest extends scaleUpTest {
+    def gen(scaleUpFactor: Int): IndexedSeq[Actor] = {
+        val markets: Int = 1
+        val traders: Int = baseFactor * scaleUpFactor - 1
+        val initialStockPrice: Double = 100
+        val budget: Double = 1000
+        val interestRate = 0.0001
+        val graph = GraphFactory.bipartite(markets, traders)
+        val cells: Map[Int, BSP with ComputeMethod] = 
+            ((0 until markets).map(i => {
+                (i, new MarketAgent(i, (markets until markets + traders), initialStockPrice))
+            }) ++ (0 until traders).map(i => {
+                (i + markets, new TraderAgent(markets + i, (0 until markets), budget, interestRate))
+            })).toMap
+        cells.values.map(i => bspToAgent(i)).toVector
+    }
+}
+
+class ERMUnoptTest extends EpidemicsUnoptTest {
+    val p = 0.01
+
+    def gen(scaleUpFactor: Int): IndexedSeq[Actor] = {
+        val graph = GraphFactory.erdosRenyi(baseFactor * scaleUpFactor, p)
+        genPopulation(toGraphInt(graph.adjacencyList())).map(i => bspToAgent(i))
+    }
+}
+
+class SBMUnoptTest extends EpidemicsUnoptTest {
+    val p =0.01
+    val q = 0
+    val numBlocks = 5
+
+    def gen(scaleUpFactor: Int): IndexedSeq[Actor] = {
+        val graph = GraphFactory.stochasticBlock(baseFactor * scaleUpFactor, p, q, numBlocks)
+        genPopulation(toGraphInt(graph.adjacencyList())).map(i => bspToAgent(i))
+    }
+}
---
+++ library/src/main/scala/BSPModel/connector/bspToAgent.scala
@@ -0,0 +1,30 @@
+package BSPModel
+
+import meta.runtime.{Message, IntMessage, DoubleMessage, DoubleVectorMessage, Actor}
+object bspToAgent{
+    def toOutMessage[T](x: T): meta.runtime.Message = {
+        x match {
+            case x: Int => new IntMessage(x)
+            case x: Double => new DoubleMessage(x)
+            case x: Vector[Double] => new DoubleVectorMessage(x)
+        }
+    }
+
+    def toInMessage[T](x: meta.runtime.Message): T = {
+        x.value.asInstanceOf[T]
+    }
+
+    def apply(bsp: BSP with ComputeMethod): meta.runtime.Actor = {
+        new meta.runtime.Actor {
+            id = bsp.id 
+            override def run(): Int = {
+                bsp.run(receivedMessages.map(i => bsp.deserialize(toInMessage[bsp.SerializeFormat](i))))
+                val outMessage = toOutMessage(bsp.message())
+                bsp.receiveFrom.foreach(n => {
+                    sendMessage(n, outMessage)
+                })
+                1
+            }
+        }
+    }
+}
